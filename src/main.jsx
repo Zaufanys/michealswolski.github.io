53c6a1856856c4fc146a3d1941d07a9f729e8961
Zaufanys <71640308+Zaufanys@users.noreply.github.com>
2026-08-06 04:27:40 -0400

    Switch site entry point to rebuilt portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Portfolio from './Portfolio'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Portfolio />
   </React.StrictMode>,
 )
